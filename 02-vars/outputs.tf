00c609c62bb5ac9a13e5bb52753cebacb8495222
tejap <btejapramod@gmail.com>
2022-02-21 14:14:03 +0530

    New Changes

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -1,3 +1,11 @@
 output "sample" {
   value = var.sample
 }
+
+output "list1" {
+  value = var.list[0]
+}
+
+output "list2" {
+  value = var.list[1]
+}
--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -4,3 +4,22 @@ variable "sample" {
   default = "NewHello"
 }
 
+#String Datatype
+variable "sample1" {
+  default = "Hello World"
+}
+##Single quotes are not supported in Terraform
+
+#Number Datatype
+variable "sample2" {
+  default = 100
+}
+
+#Boolean Datatype
+variable "sample3" {
+  default = true
+}
+
+variable "list" {
+  default = ["Hello World", 90, true]
+}
