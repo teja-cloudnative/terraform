ad0513ddf9f002e352a3c14750960d35e34c2a66
tejap <btejapramod@gmail.com>
2022-02-21 15:06:51 +0530

    New Changes

---
+++ examples/01-sg-with-ec2/ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "web" {
+  ami           = "ami-067ce86d1f553a611"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
