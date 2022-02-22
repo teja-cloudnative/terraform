5c9d569cfb9f429da62f1ef21b9bf1fb9c547405
tejap <btejapramod@gmail.com>
2022-02-22 20:37:11 +0530

    New Changes

--- roboshop/module/ec2.tf
+++ roboshop/module/ec2.tf
@@ -7,7 +7,7 @@
 #  }
 #}
 
-resource "aws_spot_instance_request" "web" {
+resource "aws_spot_instance_request" "cheap_worker" {
   ami                    = "ami-067ce86d1f553a611"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_app.id]
