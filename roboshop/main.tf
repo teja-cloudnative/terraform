69d16c26f215df405fe241ac568d2b4c4006b761
tejap <btejapramod@gmail.com>
2022-02-22 23:11:05 +0530

    New Changes

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,30 +1,37 @@
-module "frontend" {
-  source = "./frontend"
-}
-module "cart" {
-  source = "./cart"
-}
-module "catalogue" {
-  source = "./catalogue"
-}
-module "mongodb" {
-  source = "./mongodb"
-}
-module "mysql" {
-  source = "./mysql"
-}
-module "payment" {
-  source = "./payment"
-}
-module "rabbitmq" {
-  source = "./rabbitmq"
-}
-module "redis" {
-  source = "./redis"
-}
-module "shipping" {
-  source = "./shipping"
-}
-module "user" {
-  source = "./user"
+#module "frontend" {
+#  source = "./frontend"
+#}
+#module "cart" {
+#  source = "./cart"
+#}
+#module "catalogue" {
+#  source = "./catalogue"
+#}
+#module "mongodb" {
+#  source = "./mongodb"
+#}
+#module "mysql" {
+#  source = "./mysql"
+#}
+#module "payment" {
+#  source = "./payment"
+#}
+#module "rabbitmq" {
+#  source = "./rabbitmq"
+#}
+#module "redis" {
+#  source = "./redis"
+#}
+#module "shipping" {
+#  source = "./shipping"
+#}
+#module "user" {
+#  source = "./user"
+#}
+
+module "roboshop" {
+  source    = "./module"
+  COMPONENT = var.COMPONENTS
+  PORT      = var.PORTS
+  MONITOR   = var.MONITOR
 }
