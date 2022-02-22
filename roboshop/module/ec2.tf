18bc71b5c3abd253c591a15108ffdafab79b8166
tejap <btejapramod@gmail.com>
2022-02-22 20:38:16 +0530

    New Changes

--- roboshop/module/ec2.tf
+++ roboshop/module/ec2.tf
@@ -11,6 +11,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                    = "ami-067ce86d1f553a611"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_app.id]
+  wait_for_fulfillment   = true
 
   tags = {
     Name = var.COMPONENT
