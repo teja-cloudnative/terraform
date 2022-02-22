8d2f431a60ffc19162c152b51b4f3b2c3172a065
tejap <btejapramod@gmail.com>
2022-02-22 21:43:40 +0530

    New Changes

--- roboshop/module/ec2.tf
+++ roboshop/module/ec2.tf
@@ -18,6 +18,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   }
 }
 
+
 resource "aws_ec2_tag" "ec2_name_tag" {
   resource_id = aws_spot_instance_request.cheap_worker.spot_instance_id
   key         = "Name"
@@ -27,4 +28,20 @@ resource "aws_ec2_tag" "ec2_monitor_tag" {
   resource_id = aws_spot_instance_request.cheap_worker.spot_instance_id
   key         = "Monitor"
   value       = var.MONITOR
+}
+
+resource"null_resource" "ansible-apply"{
+  triggers = {
+    abc = timestamp()
+  }
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_spot_instance_request.cheap_worker.private_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+    inline = [
+      "ansible-pull -U https://github.com/teja-cloudnatice/ansible roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=dev"
+    ]
+  }
 }
